48f49885b8bbfc06b3fc67662f07924c397516a6
Rafael Ribeiro <75400435+Rafa070@users.noreply.github.com>
2022-12-16 22:58:37 -0300

    Update settings.py
    v2

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '86EiGEw0s0cKKs5KaxWMrlusSkXqimuj01r8GNo9d7MzEW5LQZPWBssYgDM5'
+SECRET_KEY = 'MRzwwMcujPciTwDwF8ffkZyxPfD1Lp7o9xYYLfupkPilK5YPJ0uKsWWy3W6m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://appsupermarkett.azurewebsites.net']
+ALLOWED_HOSTS = ['https://appsupermarket012.azurewebsites.net']
 
 
 # Application definition
